d15239a80abb86aca4fd923172f290235f1aa1a3
sadaking <sichirigahama.37@gmail.com>
2020-07-09 09:14:08 +0900

    マイグレーションファイルを修正(Trackableとnull: falseの追加)

--- db/migrate/20200708235701_devise_create_user_students.rb
+++ db/migrate/20200708235701_devise_create_user_students.rb
@@ -15,11 +15,11 @@ class DeviseCreateUserStudents < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,9 +32,9 @@ class DeviseCreateUserStudents < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.integer :student_id
-      t.integer :school_id
-      t.string :name
+      t.integer :student_id, null: false
+      t.integer :school_id, null: false
+      t.string :name, null: false
 
       t.timestamps null: false
     end
--- db/migrate/20200708235857_devise_create_user_officers.rb
+++ db/migrate/20200708235857_devise_create_user_officers.rb
@@ -15,11 +15,11 @@ class DeviseCreateUserOfficers < ActiveRecord::Migration[5.2]
       t.datetime :remember_created_at
 
       ## Trackable
-      # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
-      # t.datetime :last_sign_in_at
-      # t.inet     :current_sign_in_ip
-      # t.inet     :last_sign_in_ip
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.inet     :current_sign_in_ip
+      t.inet     :last_sign_in_ip
 
       ## Confirmable
       # t.string   :confirmation_token
@@ -32,9 +32,9 @@ class DeviseCreateUserOfficers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.integer :type
+      t.integer :type, null: false
       t.integer :school_id
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps null: false
     end
