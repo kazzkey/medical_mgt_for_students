dd56f9f98ef655f62127657d369a6e2ccc5a09e5
sadaking <sichirigahama.37@gmail.com>
2020-07-09 10:46:06 +0900

    教師、医師の選択をするenumの追加

--- app/models/user_officer.rb
+++ app/models/user_officer.rb
@@ -3,4 +3,6 @@ class UserOfficer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum type: {教師:0, 医師:1}
 end
