a3fbe55ba98155b33857f1ea1078345d2f84ee79
Kazuki Kawano <k_kawano86@yahoo.co.jp>
2020-07-29 21:21:02 +0900

    画像アップロード機能を実装

--- app/models/contact_student.rb
+++ app/models/contact_student.rb
@@ -1,5 +1,6 @@
 class ContactStudent < ApplicationRecord
   belongs_to :user_student
+  has_one_attached :image
 
   validates :title  , presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65535 }
