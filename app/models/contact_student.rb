9528ad49d000e91961b08c804548157159f1350b
sadaking <sichirigahama.37@gmail.com>
2020-08-07 13:32:28 +0900

    student_commentとのアソシエーション

--- app/models/contact_student.rb
+++ app/models/contact_student.rb
@@ -1,6 +1,7 @@
 class ContactStudent < ApplicationRecord
   belongs_to :user_student
   has_one_attached :image
+  has_many :student_comments, dependent: :destroy
 
   validates :title  , presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 65535 }
