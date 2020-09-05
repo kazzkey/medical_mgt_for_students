a16186b802a89305d49470081ee68a69f0dff764
sadaking <sichirigahama.37@gmail.com>
2020-09-05 23:21:54 +0900

    生徒側しかマイページに入れないようにした

--- app/controllers/user_students_controller.rb
+++ app/controllers/user_students_controller.rb
@@ -1,5 +1,12 @@
 class UserStudentsController < ApplicationController
+  before_action :authenticate_user_student!
+
+  def index
+    @user_students = UserStudent.all
+  end
+
   def show
+    @checks = current_user_student.checks
     @user_student = current_user_student
     @contacts = Contact.where(user_student_id: @user_student.id).all.page(params[:page]).per(10)
     @user_student = UserStudent.find(params[:id])
@@ -7,4 +14,6 @@ class UserStudentsController < ApplicationController
       redirect_to homes_path, notice: '他の人のプロフィール画面へはアクセスが出来ません。'
     end
   end
+
+
 end
